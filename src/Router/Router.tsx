3834abda566f5068931cb00a2b9ceafdfe151963
Srishameli46 <srishameli.nahamani@ideas2it.com>
2024-09-27 18:39:27 +0530

    Feat : Initialised base setup

---
+++ src/Router/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Outlet, Routes} from 'react-router-dom';
+
+function Router() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+    </Routes>
+    </BrowserRouter>
+    < Outlet />
+    </>
+  )
+}
+
+export default Router
